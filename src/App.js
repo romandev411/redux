1f4b85a9a2c828a135124a53afb7e34c5c7b22e6
romandev411 <romandev411@gmail.com>
2022-06-07 17:39:06 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
-import { connect } from 'react-redux'
-import { increment, decrement, changeStep } from './actions/actionCreator'
+import Counter from './components/Counter/Counter'
 
 function App (props) {
-  const inc = () => {
-    props.increment()
-  }
-
-  const dec = () => {
-    props.decrement()
-  }
-
-  const inputChange = e => {
-    props.changeStep(e.target.value)
-  }
-
   return (
     <div className='App'>
-      <button onClick={inc}>+</button>
-      {props.count}
-      <button onClick={dec}>-</button>
-      <input type='number' value={props.step} onChange={inputChange} />
-      {props.step}
+      <Counter></Counter>
     </div>
   )
 }
 
-const mapStateToProps = state => {
-  return {
-    count: state.count,
-    step: state.step
-  }
-}
-
-const wrappedApp = connect(mapStateToProps, {
-  increment,
-  decrement,
-  changeStep
-})(App)
-
-export default wrappedApp
+export default App
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,41 @@
+import { connect } from 'react-redux'
+import { increment, decrement, changeStep } from '../../actions/actionCreator'
+
+function Counter (props) {
+  const inc = () => {
+    props.increment()
+  }
+
+  const dec = () => {
+    props.decrement()
+  }
+
+  const inputChange = e => {
+    props.changeStep(e.target.value)
+  }
+
+  return (
+    <div className='Counter'>
+      <button onClick={inc}>+</button>
+      {props.count}
+      <button onClick={dec}>-</button>
+      <input type='number' value={props.step} onChange={inputChange} />
+      {props.step}
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    count: state.count,
+    step: state.step
+  }
+}
+
+const wrappedApp = connect(mapStateToProps, {
+  increment,
+  decrement,
+  changeStep
+})(Counter)
+
+export default wrappedApp
